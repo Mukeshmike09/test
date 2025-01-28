6e81a20f5e84027486ecaa00f9dab9f44230d2b9
Mukeshmike09 <39398208+Mukeshmike09@users.noreply.github.com>
2025-01-29 00:04:11 +0530

    Update network_test_runner.py

--- network_test_runner.py
+++ network_test_runner.py
@@ -1,7 +1,7 @@
 import subprocess
 import argparse
 import yaml
-#test2
+#test2   tyh
 def run_command(command, *args):
 
     print("=============================================================".center(100, ' '))
